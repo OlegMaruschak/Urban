4249169df95b70778c5c8fecf273979015283415
OlegMaruschak <maruschak.oa@gmail.com>
2025-01-27 23:35:49 +0300

    Д/з по теме Генераторы
    Домашнее задание по теме "Генераторы"

---
+++ module_9_6.py
@@ -0,0 +1,12 @@
+# Домашнее задание по теме "Генераторы"
+
+
+def all_variants(text):
+    for x in range(len(text)):
+        for r in range(len(text) - x):
+            yield text[x:r + x + 1]
+
+
+a = all_variants("abc")
+for i in a:
+    print(i)
